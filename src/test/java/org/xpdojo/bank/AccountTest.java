91d16b00cef3465fb33fc4ffd909b02b7f09f30a
Nazeeya S <shikalgar.nazeeya@gmail.com>
2022-01-28 19:19:08 +0530

    Refactoring on TDD

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,28 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.emptyAccount;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void initialAccount_shouldHaveZeroBalance() {
+
+        assertThat(emptyAccount().balance()).isEqualTo(0);
+    }
+    @Test
+    public void depositAmount_shouldIncreaseBalanceAmount(){
+        Account account = emptyAccount();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+
+    }
+
+    @Test
+    public void depositMultipleAmount(){
+        Account account = emptyAccount();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
     }
 }
